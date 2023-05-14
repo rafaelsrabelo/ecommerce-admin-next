f684da77889433be6b9c7c5e5bde48f75f960347
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-14 03:18:09 -0300

    style(Nav.js): fix camelCase naming of stroke-width attribute to strokeWidth style(Nav.js): fix camelCase naming of stroke-linecap attribute to strokeLinecap style(Nav.js): fix camelCase naming of stroke-linejoin attribute to strokeLinejoin
    style(Nav.js): change SVG attributes from camelCase to kebab-case to follow React's naming convention and improve readability
    
    feat(new.js): add form to create new product with input fields for title, description and price and submit button. Add state hooks to manage input values.

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,24 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct () {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    function createProduct() {
+    }
     return (
         <Layout>
-            <h1>New Product</h1>
-            <label>Product name</label>
-            <input type="text" placeholder="product name" />
-            <label>Description</label>
-            <textarea placeholder="description"></textarea>
-            <label>Price (in USD)</label>
-            <input type="number" placeholder="price" />
-            <button className="btn-primary">Save</button>
+            <form onSubmit={() => console.log(event.target)}>
+                <h1>New Product</h1>
+                <label>Product name</label>
+                <input type="text" value={title} placeholder="product name" onChange={event => setTitle(event.target.value)}/>
+                <label>Description</label>
+                <textarea value={description} placeholder="description" onChange={event => setDescription(event.target.value)}/>
+                <label>Price (in USD)</label>
+                <input type="number" value={price} placeholder="price" onChange={event => setPrice(event.target.value)}/>
+                <button type="submit" className="btn-primary">Save</button>
+            </form>
         </Layout>
     )
 }
