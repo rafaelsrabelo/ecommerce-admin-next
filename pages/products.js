dd541d6ee48ab68906cce0943cc38958f956880b
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-14 02:46:23 -0300

    feat(products.js, new.js): add new product page and link to it from products page

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,10 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
     return (
         <Layout>
-            Products page here
+            <Link className="bg-blue-900 text-white py-1 px-2 rounded-md" href='/products/new'>Add new product</Link>
         </Layout>
     )
 }
