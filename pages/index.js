f04e23f2ba801cacb4ceafe5a941392314e951b6
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-14 02:11:26 -0300

    feat(Layout.js): create new component Layout to wrap pages and add authentication logic refactor(index.js): replace Nav and authentication logic with Layout component in Home page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,9 @@
-import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react"
-export default function Home() {
-  const { data: session } = useSession();
-  if(!session) {
-    return (
-      <div className='bg-blue-900 w-screen flex items-center h-screen'>
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
+import Layout from "@/components/Layout";
 
-return (
-  <div className="bg-blue-900 min-h-screen flex">
-    <Nav />
-    <div className="bg-white flex-grow mt-2 mr-2 mb-0 rounded-lg p-4">logged in {session.user.email}</div>
-    {/* <button onClick={() => { signOut()}} className="bg-red-500 text-white p-2 px-4 rounded-lg">Sair</button> */}
-  </div>
-)
+export default function Home() {
+  return (
+    <Layout>
+      Teste
+    </Layout>
+  )
 }
