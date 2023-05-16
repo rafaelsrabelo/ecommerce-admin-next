a0263c598a2940d1838116d6e8ad09be463a46f4
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-16 11:03:17 -0300

    empty product

--- components/Nav.js
+++ components/Nav.js
@@ -3,13 +3,13 @@ import { useRouter } from "next/router";
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Nav () {
-    const inactiveLink = 'flex gap-1 p-1';
-    const activeLink = inactiveLink+' bg-white text-gray-900 rounded-l-lg';
+    const inactiveLink = 'flex gap-1 px-4 py-2';
+    const activeLink = inactiveLink+' bg-white text-blue-900 font-bold';
     const router = useRouter();
     const {pathname} = router;
     return (
-        <aside className="text-white p-4 pr-0">
-            <Link href={'/'} className="flex gap-1 mb-8 mr-4">
+        <aside className="text-white p-4 pr-0 ps-0 justify-center ">
+            <Link href={'/'} className="flex gap-1 mb-8 mr-4 px-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
                 </svg>
@@ -41,7 +41,7 @@ export default function Nav () {
                 </svg>
                 Configurações
                 </Link>
-                <button onClick={() => { signOut()}} className="flex p-1 mt-10">
+                <button onClick={() => { signOut()}} className="flex mt-20 px-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 me-1">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                 </svg>
--- pages/products.js
+++ pages/products.js
@@ -2,6 +2,8 @@ import Layout from "@/components/Layout";
 import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import empty from "./../public/empty.svg";
+import Image from "next/image";
 
 export default function Products() {
     const [products, setProducts] = useState([]);
@@ -18,25 +20,34 @@ export default function Products() {
                 Adicionar produto
             </Link>
             {
-                products.map(product => (
-                    <div key={product._id} className="flex items-center border border-gray-300 rounded-md p-4 mt-4">
-                        <h4 className="text-lg font-bold p-0">{product.title}</h4>
-                        <div className="flex align-center ms-auto">
-                            <Link  href={'/products/edit/'+product._id} className="flex align-center text-white rounded-lg p-2 bg-gray-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 me-2">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                            </svg>
-                            Edit
-                            </Link>
-                            <Link  href={'/products/delete/'+product._id} className="flex align-center text-white rounded-lg p-2 ms-4 bg-red-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 me-2">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                            </svg>
-                            Delete
-                            </Link>
+                products.length === 0 ? (
+                    <h1 className="flex items-center h-screen justify-center">
+                            <div>
+                                <Image src={empty} alt="empty" className="object-contain h-48 w-96 mb-5" />
+                                <p className="">Não há produtos cadastrados, adicione um produto!</p>
+                            </div>
+                    </h1>
+                ) : (
+                    products.map(product => (
+                        <div key={product._id} className="flex items-center border border-gray-300 rounded-md p-4 mt-4">
+                            <h4 className="text-lg font-bold p-0">{product.title}</h4>
+                            <div className="flex align-center ms-auto">
+                                <Link href={'/products/edit/' + product._id} className="flex align-center text-white rounded-lg p-2 bg-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 me-2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                    </svg>
+                                    Edit
+                                </Link>
+                                <Link href={'/products/delete/' + product._id} className="flex align-center text-white rounded-lg p-2 ms-4 bg-red-600">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 me-2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                    </svg>
+                                    Delete
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                ))
+                    ))
+                )
             }
         </Layout>
     )
