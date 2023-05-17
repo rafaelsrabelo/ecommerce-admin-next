063d389dceaa03d52dc8ff76702cacfb1d10dfc5
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-17 13:11:40 -0300

    feat(Category.js, categories.js, api/categories.js): add support for parent categories to create a hierarchy of categories fix(api/categories.js): fix GET method to populate parent category fix(products.js): fix typo in Edit and Delete buttons text

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,10 +1,25 @@
+import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/Category";
 
 export default async function handle(req, res) {
     const method = req.method;
+    await mongooseConnect();
+    if( method === 'GET') {
+        res.json(await Category.find().populate('parent'));
+    }
     if(method === 'POST') {
-        const {name} = req.body;
-        const categoryDoc = await Category.create({name});
+        const {name, parentCategory} = req.body;
+        const categoryDoc = await Category.create({
+            name, 
+            parent: parentCategory || undefined});
+        res.json({categoryDoc})
+    }
+
+    if(method === 'PUT')   {
+        const {name, parentCategory, _id} = req.body;
+        const categoryDoc = await Category.updateOne({_id},{
+            name, 
+            parent: parentCategory || undefined});
         res.json({categoryDoc})
     }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -1,24 +1,101 @@
 import Layout from "@/components/Layout";
 import axios from "axios";
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function CategoriesPage() {
+    const [editedCategory, setEditedCategory] = useState(null);
     const [name, setName] = useState('');
-    
+    const [categories, setCategories] = useState([]);
+    const [parentCategory, setParentCategory] = useState('');
+
+    useEffect(() => {
+        fetchCategories();
+    }, [])
+
+    function fetchCategories() {
+        axios.get('/api/categories').then(result => {
+            setCategories(result.data);
+        })
+    }
     async function saveCategory(ev) {
         ev.preventDefault();
-        await axios.post('/api/categories', {name});
-        setName('')
+        const data = {
+            name, parentCategory
+        }
+        if(editedCategory) {
+            data._id = editedCategory._id;
+            await axios.put('/api/categories', data);
+            setEditedCategory(null);
+        } else {
+            await axios.post('/api/categories', data);
+        }
+        setName('');
+        fetchCategories();
+    }
+
+    function editCategory(category) {
+        setEditedCategory(category);
+        setName(category.name);
+        setParentCategory(category?.parent?._id);
     }
 
     return (
         <Layout>
             <h1>Categorias</h1>
-            <label>Nova categoria</label>
+            <label>{editedCategory ? `Editar categoria: ${editedCategory.name}` : 'Criar categoria'}</label>
             <form onSubmit={saveCategory} className="flex gap-1">
-                <input value={name} onChange={ev => setName(ev.target.value)} placeholder="Nome da categoria" type="text" className="mb-0"/>
+                <input value={name} onChange={ev => setName(ev.target.value)} placeholder="Nome da categoria" type="text" className="mb-0" />
+                <select className="mb-0"  value={parentCategory} onChange={ev => setParentCategory(ev.target.value)}>
+                    <option value=''>Categoria parente</option>
+                    {
+                        categories.length > 0 && categories.map(category => (
+                            <option key={category._id} value={category._id}>{category.name}</option>
+                        ))
+                    }
+                </select>
                 <button className="btn-primary">Salvar</button>
             </form>
+
+            <div className="bg-blue-200 text-black px-2 py-4 my-4 rounded-lg">
+                Você pode criar categorias para organizar seus itens de forma mais eficiente. Ao 
+                criar uma categoria, você pode optar por associá-la a uma categoria parente 
+                existente, o que ajuda a estabelecer uma hierarquia e relacionamentos entre as 
+                categorias.
+            </div>
+
+            <div className="flex justify-between ps-2 pe-4">
+                <p>Nome da categoria</p>
+                <p></p>
+                <p></p>
+            </div>
+            <hr className="mb-4" />
+            {
+                categories.length > 0 && categories.map(category => (
+                    <div className="flex justify-between ps-2 pe-4 mb-2" key={category._id}>
+                        <h4>{category.name}</h4>
+                        <small>{category?.parent?.name}</small>
+                        <div className="flex align-center">
+                                <Link
+                                    href=''
+                                    onClick={() => editCategory(category)} 
+                                    className="flex align-center text-white rounded-lg p-2 bg-gray-500"
+                                    >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 me-2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                    </svg>
+                                    Editar
+                                </Link>
+                                <Link href='/' className="flex align-center text-white rounded-lg p-2 ms-4 bg-red-600">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 me-2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                    </svg>
+                                    Deletar
+                                </Link>
+                            </div>
+                    </div>
+                ))
+            }
         </Layout>
     )
 }
